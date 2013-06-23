292e4f980b8cf752a4017ac4055769ac4e73aab6
dreamlx <dreamlinx@gmail.com>
2013-06-23 20:09:41 +0800

    add service name

--- app/assets/javascripts/girls.js
+++ app/assets/javascripts/girls.js
@@ -22,17 +22,22 @@ $(function() {
         return Number(this.value);
     }).get();
     var sum = Number(0);
+    var service_detial = "";
     services = eval(gon.services);
     //根据service.id 匹配checkbox，获得服务价格
     for (var i = 0, sum = 0; i < checkedVals.length; i++){
       for( var j = 0; j < services.length; j++){
         if(services[j].id == checkedVals[i]){
           sum += Number(services[j].service_price);
+          service_detial += services[j].service_name + ":" + services[j].service_price;
+          service_detial +=", ";
+          
         }
       }
     };
 
     $("#task_price").val(sum);
+    $("#task_title").val(service_detial);
   });
 });
 
