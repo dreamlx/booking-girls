376c491b64f75e7439c317952bd0ea85e0e2aa1a
dreamlx <dreamlinx@gmail.com>
2013-06-23 20:52:44 +0800

    modified:   app/models/service.rb

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,10 +1,14 @@
 class Service < ActiveRecord::Base
-  attr_accessible :price, :service_name
+  attr_accessible :service_price, :service_name
 
   has_and_belongs_to_many :tasks
 
   def name_with_price
-    "#{self.service_name}:#{sprintf("%05.2f", self.service_price)}"
+    if !self.service_price.nil? and !self.service_name.nil?
+      "#{self.service_name}:#{sprintf("%05.2f", self.service_price)}"
+    else
+      return 0
+    end
   end
 
 end
