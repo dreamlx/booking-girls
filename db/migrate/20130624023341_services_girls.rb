b474867a905011b8d9e3ed7ea99213c180a6040d
dreamlx <dreamlinx@gmail.com>
2013-06-25 16:32:32 +0800

    modified:   db/migrate/20130624023341_services_girls.rb

--- db/migrate/20130624023341_services_girls.rb
+++ db/migrate/20130624023341_services_girls.rb
@@ -1,8 +1,8 @@
 class ServicesGirls < ActiveRecord::Migration
   def change
-    create_table :girl_services do |t|
+    create_table :girls_services do |t|
       t.references :girl
-      t.references :service
+      t.references :service_menu
     end
     add_index :girls_services, :girl_id
     add_index :girls_services, :service_id
