5ecd2c4ccf4c3ed9d0950991394a8cb9b8f4aec2
dreamlx <dreamlinx@gmail.com>
2013-06-25 18:18:25 +0800

    deleted:    db/migrate/20130625073247_rename_table_service_to_service_menu.rb

--- db/migrate/20130625073247_rename_table_service_to_service_menu.rb
+++
@@ -1,6 +0,0 @@
-class RenameTableServiceToServiceMenu < ActiveRecord::Migration
-  def change
-    rename_table :services, :service_menus
-    rename_table :girls_services, :girls_service_menus
-  end
-end
